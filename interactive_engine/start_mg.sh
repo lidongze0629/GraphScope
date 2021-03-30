655840dad3abbed0f8595c299ae045b6f196ca9c
Nathaniel-Han <yuxinghn@gmail.com>
2021-03-30 11:33:17 +0800

    add start_mg.sh

--- interactive_engine/start_mg.sh
+++ interactive_engine/start_mg.sh
@@ -2,7 +2,20 @@
 
 set -x
 
-#bash deploy/shell/local_deploy.sh build all debug
+
+ROOT_DIR=$PWD
+wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz
+tar xf zookeeper-3.4.14.tar.gz -C /usr/local/
+cd /usr/local
+ln -s zookeeper-3.4.14 zookeeper
+mkdir -p /usr/local/zookeeper/data
+mkdir -p /usr/local/zookeeper/logs
+
+/usr/local/zookeeper/bin/zkServer.sh start
+
+cd $ROOT_DIR
+
+bash deploy/shell/local_deploy.sh build all debug
 
 bash ./start_coordinator.sh
 sleep 10
