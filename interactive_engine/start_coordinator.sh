70d17c65815a4b444cbdb813f0a88301daab5567
Nathaniel-Han <yuxinghn@gmail.com>
2021-03-30 14:50:37 +0800

    fix

--- interactive_engine/start_coordinator.sh
+++ interactive_engine/start_coordinator.sh
@@ -5,6 +5,8 @@ rm -rf /home/maxgraph/logs/coordinator
 mkdir /home/maxgraph/logs/coordinator
 mkdir -p $ROOT_DIR/logs/
 
+object_id=26758028691315914
+
 JAVA_OPT="-server -Xmx1024m -Xms1024m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./java.hprof -verbose:gc -Xloggc:./gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution -Djava.awt.headless=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=30000 -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=75 -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dlogfilename=${ROOT_DIR}/logs/maxgraph-coordinator.log -Dlogbasedir=/home/maxgraph/logs/coordinator -Dlog4j.configurationFile=file:${ROOT_DIR}/build/0.0.1-SNAPSHOT/conf/log4j2.xml -classpath ${ROOT_DIR}/build/0.0.1-SNAPSHOT/conf/*:${ROOT_DIR}/build/0.0.1-SNAPSHOT/lib/*:"
 
 inner_config=$ROOT_DIR/deploy/docker/dockerfile/coordinator.application.properties
