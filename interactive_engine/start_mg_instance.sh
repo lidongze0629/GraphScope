32dbf6b65866b8e896bf357f58858e192b7a5c8a
棋昱 <yuxing.hyx@alibaba-inc.com>
2021-03-30 21:00:44 +0800

    add start_mg_instance.sh

---
+++ interactive_engine/start_mg_instance.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+ROOT_DIR=$PWD
+#wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz
+#tar xf zookeeper-3.4.14.tar.gz -C /usr/local/
+#cd /usr/local
+#ln -s zookeeper-3.4.14 zookeeper
+#mkdir -p /usr/local/zookeeper/data
+#mkdir -p /usr/local/zookeeper/logs
+#
+#/usr/local/zookeeper/bin/zkServer.sh start
+#cd $ROOT_DIR
+
+#bash deploy/shell/local_deploy.sh build all debug
+
+object_id=$1
+schema_path=$2
+server_id=$3
+VINEYARD_IPC_SOCKET=$4
+
+bash ./start_coordinator.sh $object_id
+sleep 10
+bash ./start_frontend.sh $object_id $schema_path
+sleep 10
+bash ./start_executor.sh $object_id $server_id $VINEYARD_IPC_SOCKET
+
