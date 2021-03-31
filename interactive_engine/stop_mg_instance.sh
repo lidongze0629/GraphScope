24ecfd24082b2b63293111d69423a9fcd153a8f9
棋昱 <yuxing.hyx@alibaba-inc.com>
2021-03-31 14:57:58 +0800

    update

--- interactive_engine/stop_mg_instance.sh
+++ interactive_engine/stop_mg_instance.sh
@@ -2,9 +2,11 @@
 
 cd ~/yuxing.hyx/tmp/GraphScope/interactive_engine
 
-coordinator_id=`cat coordinator.pid`
-frontend_id=`cat frontend.pid`
-executor_id=`cat executor.pid`
+object_id=$1
+
+coordinator_id=`cat coordinator_${object_id}.pid`
+frontend_id=`cat frontend_${object_id}.pid`
+executor_id=`cat executor_${object_id}.pid`
 
 sudo kill -9 $coordinator_id
 sudo kill -9 $frontend_id
