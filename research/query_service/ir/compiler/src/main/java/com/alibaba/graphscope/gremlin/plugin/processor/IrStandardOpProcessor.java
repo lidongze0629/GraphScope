42d3a592cb08b82d46cb5f2789c11bbd79d4e98b
lidongze0629 <9546726@qq.com>
2022-06-02 21:07:50 +0800

    update

--- research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/gremlin/plugin/processor/IrStandardOpProcessor.java
+++ research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/gremlin/plugin/processor/IrStandardOpProcessor.java
@@ -115,7 +115,7 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
         Bindings bindings = new SimpleBindings();
 
         GremlinExecutor.LifeCycle lifeCycle =
-                createLifeCycle(ctx, gremlinExecutorSupplier, bindingsSupplier);
+                createLifeCycle(ctx, gremlinExecutorSupplier, bindingsSupplier, script);
 
         try {
             CompletableFuture<Object> evalFuture =
@@ -234,7 +234,7 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
     protected GremlinExecutor.LifeCycle createLifeCycle(
             Context ctx,
             Supplier<GremlinExecutor> gremlinExecutorSupplier,
-            BindingSupplier bindingsSupplier) {
+            BindingSupplier bindingsSupplier, String script) {
         final RequestMessage msg = ctx.getRequestMessage();
         final Settings settings = ctx.getSettings();
         final Map<String, Object> args = msg.getArgs();
@@ -270,7 +270,7 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
                                     processTraversal(
                                             traversal,
                                             new GremlinResultProcessor(
-                                                    ctx, GremlinResultAnalyzer.analyze(traversal)));
+                                                    ctx, GremlinResultAnalyzer.analyze(traversal)), script);
                                 }
                             } catch (InvalidProtocolBufferException e) {
                                 throw new RuntimeException(e);
@@ -281,9 +281,12 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
                 .create();
     }
 
-    protected void processTraversal(Traversal traversal, ResultProcessor resultProcessor)
+    protected void processTraversal(Traversal traversal, ResultProcessor resultProcessor, String script)
             throws InvalidProtocolBufferException, IOException, RuntimeException {
         IrMeta irMeta = metaQueryCallback.beforeExec();
+
+        long jobId = JOB_ID_COUNTER.incrementAndGet();
+        String jobName = script + "_" + jobId;
 
         InterOpCollection opCollection = (new InterOpCollectionBuilder(traversal)).build();
         // fuse order with limit to topK
@@ -292,13 +295,19 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
         InterOpCollection.process(opCollection);
 
         IrPlan irPlan = new IrPlan(irMeta, opCollection);
-        logger.info("{}", irPlan.getPlanAsJson());
+        logger.info("job is {}, plan is {}", jobName, irPlan.getPlanAsJson());
 
         byte[] physicalPlanBytes = irPlan.toPhysicalBytes(configs);
         irPlan.close();
 
-        long jobId = JOB_ID_COUNTER.incrementAndGet();
-        String jobName = "ir_plan_" + jobId;
+        // long jobId = JOB_ID_COUNTER.incrementAndGet();
+        // String jobName = "ir_plan_" + jobId;
+        int serverNum = PegasusConfig.PEGASUS_SERVER_NUM.get(configs);
+        List<Long> servers = new ArrayList<>();
+        for (long i = 0; i < serverNum; ++i) {
+            servers.add(i);
+        }
+
 
         PegasusClient.JobRequest request = PegasusClient.JobRequest.parseFrom(physicalPlanBytes);
         PegasusClient.JobConfig jobConfig =
